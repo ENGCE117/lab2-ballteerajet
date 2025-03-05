e07988af84d50ed455715b54f3e506d60c4f0a9b
ballteerajet <teerajet009@gmail.com>
2025-03-05 21:55:03 +0700

    First commit

--- student_program.cpp
+++ student_program.cpp
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <string.h>
+
+char* reverse(char str1[]) {
+    int len = strlen(str1);
+    for (int i = 0; i < len / 2; i++) {
+        char temp = str1[i];
+        str1[i] = str1[len - i - 1];
+        str1[len - i - 1] = temp;
+    }
+    printf("FUNC : %s\n", str1);
+    return str1;
+}
 
 int main() {
-    //YOUR CODE HERE
-    return 0 ;
-}//end function
+    char text[50];
+    printf("Enter str1 : ");
+    fgets(text, sizeof(text), stdin);
+    text[strcspn(text, "\n")] = '\0';
+    
+    char *output = reverse(text);
+    printf("MAIN : %s\n", output);
+    
+    return 0;
+}
