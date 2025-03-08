06517bb1bbc7403bd7158125ef07bb7409a30b25
ballteerajet <126446949+ballteerajet@users.noreply.github.com>
2025-03-08 13:53:49 +0700

    Update student_program.cpp

--- student_program.cpp
+++ student_program.cpp
@@ -1,16 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-char* reverse(char str1[]) {
-    int len = strlen(str1);
-    for (int i = 0; i < len / 2; i++) {
-        char temp = str1[i];
-        str1[i] = str1[len - i - 1];
-        str1[len - i - 1] = temp;
-    }
-    printf("FUNC : %s\n", str1);
-    return str1;
-}
+char* reverse( char str1[]) ;
 
 int main() {
     char text[50];
@@ -22,4 +13,15 @@ int main() {
     printf("MAIN : %s\n", output);
     
     return 0;
+}
+
+char* reverse(char str1[]) {
+    int len = strlen(str1);
+    for (int i = 0; i < len / 2; i++) {
+        char temp = str1[i];
+        str1[i] = str1[len - i - 1];
+        str1[len - i - 1] = temp;
+    }
+    printf("FUNC : %s\n", str1);
+    return str1;
 }
